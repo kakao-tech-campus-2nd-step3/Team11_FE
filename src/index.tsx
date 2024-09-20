1544e34fb6478e0f73205282f83e2df7d969c814
JaeAnHan <jaean1999@naver.com>
2024-09-20 21:58:20 +0900

    feat: 라우팅 설정 및 eslint 설정 변경

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from '@/App';
 import reportWebVitals from '@/reportWebVitals';
+import { RouterProvider } from 'react-router';
+import { router } from '@/router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
+const queryClient = new QueryClient();
+
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
 
