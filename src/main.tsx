1ec290f64dd7254b246a38be2b230d777713f5ad
JaeAnHan <jaean1999@naver.com>
2024-09-26 22:35:05 +0900

    chore: Chakra UI 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
+import { ChakraProvider } from '@chakra-ui/react';
+
 import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </StrictMode>
 );
