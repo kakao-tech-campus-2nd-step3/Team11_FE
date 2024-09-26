ad8d28f368ebfb04ac1096a7e5401c119a643b5e
JaeAnHan <jaean1999@naver.com>
2024-09-26 22:47:11 +0900

    chore: svg 사용 가능 하도록

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import { useState } from 'react';
 
-import reactLogo from '@images/react.svg';
+import { Flex } from '@chakra-ui/react';
+import ReactLogo from '@images/react.svg?react';
 
-import './App.css';
-import viteLogo from '/vite.svg';
+import ViteLogo from '../public/vite.svg?react';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
+      <Flex>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <ViteLogo />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <ReactLogo />
         </a>
-      </div>
-      <div className="card">
+      </Flex>
+      <div>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
