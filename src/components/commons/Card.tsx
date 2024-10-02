82fb057a1d743b4707c38d3a7e5b3885720f8eb6
JaeAnHan <jaean1999@naver.com>
2024-10-02 23:25:00 +0900

    feat: 공통 컴포넌트 카드 추가

---
+++ src/components/commons/Card.tsx
@@ -0,0 +1,30 @@
+import React, { ReactNode } from 'react';
+
+import { Flex } from '@chakra-ui/react';
+
+interface ICard {
+  children: ReactNode;
+  height?: number;
+  bg?: string;
+  boxShadow?: string;
+}
+
+export const Card: React.FC = ({
+  children,
+  bg = 'white',
+  height = 150,
+  boxShadow = 'md',
+}: ICard) => (
+  <Flex
+    pl={10}
+    pr={4}
+    alignItems="center"
+    justifyContent={'space-between'}
+    borderRadius={20}
+    bg={bg}
+    h={height}
+    boxShadow={boxShadow}
+  >
+    {children}
+  </Flex>
+);
