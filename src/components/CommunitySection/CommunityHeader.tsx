6c337100f9e62211036fe818ff593992649aec3a
JaeAnHan <jaean1999@naver.com>
2024-10-03 23:15:48 +0900

    feat: 카테고리 헤더 섹션 배경 애니메이션 추가

--- src/components/CommunitySection/CommunityHeader.tsx
+++ src/components/CommunitySection/CommunityHeader.tsx
@@ -3,6 +3,9 @@ import React from 'react';
 import { BoomerangColors } from '@/utils/colors';
 import { Flex, Text } from '@chakra-ui/react';
 import CommunityIcon from '@images/community.svg?react';
+import Note from '@images/note.svg?react';
+
+import styles from './index.module.css';
 
 export const CommunityHeader = () => (
   <Flex
@@ -13,10 +16,12 @@ export const CommunityHeader = () => (
     mb={30}
     alignItems="center"
     boxShadow="xl"
+    className={styles['community--header']}
   >
     <Text fontSize="24px" mr={2} color={BoomerangColors.white}>
       커뮤니티
     </Text>
     <CommunityIcon />
+    <Note />
   </Flex>
 );
