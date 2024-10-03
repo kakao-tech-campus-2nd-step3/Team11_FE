c82c186d1f4866ab643ab6ded2e1ded72e18f0f8
JaeAnHan <jaean1999@naver.com>
2024-10-03 22:26:39 +0900

    feat: 폰트 적용
    - createHtmlPlugin을 통한 preload 적용

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,44 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { createHtmlPlugin } from 'vite-plugin-html';
 import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), svgr()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    svgr(),
+    createHtmlPlugin({
+      inject: {
+        data: {
+          preloadFonts: [
+            {
+              rel: 'preload',
+              href: '/assets/fonts/NanumSquareOTF_acR.otf',
+              as: 'font',
+              type: 'font/otf',
+              crossorigin: 'anonymous',
+            },
+            {
+              rel: 'preload',
+              href: '/assets/fonts/NanumSquareOTF_acB.otf',
+              as: 'font',
+              type: 'font/otf',
+              crossorigin: 'anonymous',
+            },
+            {
+              rel: 'preload',
+              href: '/assets/fonts/NanumSquareOTF_acEB.otf',
+              as: 'font',
+              type: 'font/otf',
+              crossorigin: 'anonymous',
+            },
+          ],
+        },
+      },
+    }),
+  ],
 });
