81a68b2e180ad5a6fdcc1573b12f821939f77bc0
yunseong0404 <333665@naver.com>
2024-10-21 23:50:46 +0900

    feature: BtnSection 구현

--- src/pages/Welcome/index.tsx
+++ src/pages/Welcome/index.tsx
@@ -22,6 +22,7 @@ export const Welcome: React.FC = () => {
     <BasicLayout maxW={1024}>
       <VStack maxW={1024} h={800} bg={'#FFF'} borderRadius={24} mt={'80px'}>
         <NicknameSection />
+        <BtnSection />
       </VStack>
     </BasicLayout>
   );
@@ -44,5 +45,36 @@ const NicknameSection = () => (
       />
       <Image src={pen} />
     </Flex>
+  </VStack>
+);
+
+const BtnSection = () => (
+  <VStack>
+    <Button
+      mt={'100px'}
+      color={'#FFF'}
+      fontSize={'22px'}
+      w={'250px'}
+      h={'80px'}
+      bg={'#176CFF'}
+      borderRadius={8}
+      shadow={'0px 0px 5px rgba(0,0,0,0.25)'}
+    >
+      일반 사용자로
+      <br />
+      가입 완료하기
+    </Button>
+    <Button
+      mt={'10px'}
+      color={'#111'}
+      fontSize={'22px'}
+      w={'250px'}
+      h={'80px'}
+      bg={'#F5F5F5'}
+      borderRadius={8}
+      shadow={'0px 0px 5px rgba(0,0,0,0.25)'}
+    >
+      멘토 회원이라면?
+    </Button>
   </VStack>
 );
