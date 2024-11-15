1c7ba0fa0ca8e92c312fb216812fa8e0e1ae9721
yunseong0404 <333665@naver.com>
2024-11-15 17:37:50 +0900

    fix: 버튼 뒤로가기 추가

--- src/components/ForumPost/PostHeader.tsx
+++ src/components/ForumPost/PostHeader.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-import { Flex, Image, Text } from '@chakra-ui/react';
-import vector from '@images/Vector.svg';
+import { Flex, IconButton, Text } from '@chakra-ui/react';
+import Vector from '@images/Vector.svg?react';
 
 interface PostHeaderProps {
   boardType?: string;
@@ -9,18 +10,28 @@ interface PostHeaderProps {
 
 export const PostHeader: React.FC<PostHeaderProps> = ({
   boardType = '자유',
-}) => (
-  <Flex
-    w="full"
-    px={{ base: 5, md: 10 }}
-    align="center"
-    justify="space-between"
-  >
-    <Flex align="center" gap={2}>
-      <Image src={vector} alt="" boxSize="18px" objectFit="contain" />
-      <Text fontSize="3xl" fontWeight="extrabold" color="#176CFF">
-        {boardType} 게시판
-      </Text>
+}) => {
+  const navigate = useNavigate();
+
+  return (
+    <Flex
+      w="full"
+      px={{ base: 5, md: 10 }}
+      align="center"
+      justify="space-between"
+    >
+      <Flex align="center" gap={2}>
+        <IconButton
+          icon={<Vector />}
+          objectFit="contain"
+          onClick={() => navigate(-1)}
+          aria-label="뒤로가기"
+          variant="unstyled"
+        />
+        <Text fontSize="3xl" fontWeight="extrabold" color="#176CFF">
+          {boardType} 게시판
+        </Text>
+      </Flex>
     </Flex>
-  </Flex>
-);
+  );
+};
